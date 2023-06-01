36facdc2fa822698068019a5e82c80387552e21f
donya <donyaroshanzamir81@gmail.com>
2023-06-01 11:14:58 +0430

    1.09

--- predictor.v
+++ predictor.v
@@ -1,15 +1,33 @@
 module predictor(input wire request, result, clk, taken, output reg prediction);
   reg previous_request;
   reg previous_result;
-
+  reg state[1:0] = 3
   always @(posedge clk) begin
     
-    previous_request <= request;
-    previous_result <= result;
-    if (previous_request && previous_result && taken)
-      prediction <= 1;
-    else
-      prediction <= 0; 
+    if (request)
+      begin
+        prediction <= state[1]    
+      end
+
+
+    if (result)
+      begin
+        if(taken)
+          begin
+            if(state <3)
+              begin
+                state<= state + 1
+              end
+          end
+        else if(taken==0)
+          begin
+            if (state>0)
+            begin
+              state<= state -1
+            end
+          end
+      end
+      
   end
 
 
